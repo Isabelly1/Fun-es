d349694f525381470ee959519f7dc86301cde9e9
Isabelly1 <isabellymartins846@gmail.com>
2022-06-26 16:13:01 -0300

    funcoes

---
+++ script.js
@@ -0,0 +1,27 @@
+function BemVindo(){
+    console.log('Bem-vindo a minha atividade de função!')
+    }
+    
+    BemVindo()
+    
+    
+    function nome (meu){
+        console.log(`Meu nome é ${meu}`)
+    }
+    nome('Isabelly')
+    
+    
+    function info(nome, idade, musica){
+    console.log(`Meu nome é ${nome}, tenho ${idade} anos e meu estilo musical é ${musica}`)
+    }
+    
+    info('Isabelly', '19', 'Kpop, internacional e gospel')
+    
+    
+    function favorito(filme, musica){
+        console.log(`${filme} é meu filme favorito, ${musica} é a música mais escutada`)
+    }
+    
+    favorito('Your Name','The One That Got Away')
+    
+    
